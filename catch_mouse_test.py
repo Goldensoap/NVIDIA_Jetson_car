fb2ca19296629317b3e823acc2ff3bfac42cc55c
Goldensoap <33715060+Goldensoap@users.noreply.github.com>
2019-07-10 22:21:08 +0800

    添加预览相机脚本，修改测试脚本中轮廓勾画的阈值

--- catch_mouse_test.py
+++ catch_mouse_test.py
@@ -8,30 +8,25 @@ def object_detection(e,d):
     """
     now = time.strftime('%Y-%m-%d_%H-%M-%S',time.localtime())
 
-    # 创建掩模
-    addimg = numpy.zeros([1080,1920],dtype=numpy.uint8)
-    mask = numpy.zeros([1080,1920],dtype=numpy.uint8)
-    mask[:,:]=255
-    mask[1012:1047,1391:1848]=0
-
     #帧计数
     counter = 0
 
     es = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (9, 4))
     background = None
 
+    #阻塞等待视频流
     e.wait()
 
-    #cv2.namedWindow('contours',0)
-    #cv2.resizeWindow('contours',640,360)
-    #cv2.namedWindow('gary',0)
-    #cv2.namedWindow('dis',0)
+    #创建窗口
+    cv2.namedWindow('contours',0)
+    cv2.resizeWindow('contours',640,360)
+    cv2.namedWindow('gary',0)
+    cv2.namedWindow('dis',0)
 
     # 设置保存名称和编码格式
     fourcc = cv2.VideoWriter_fourcc('X','2','6','4')
-    output = cv2.VideoWriter('/media/pi/OTA128/{}.avi'.format(now),fourcc,d['fps'],d['size'])
+    output = cv2.VideoWriter('{}.avi'.format(now),fourcc,d['fps'],d['size'])
 
-    # 判断视频是否打开
     while True:
 
         # 读取视频流
@@ -41,7 +36,6 @@ def object_detection(e,d):
         # 用高斯滤波进行模糊处理，进行处理的原因：每个输入的视频都会因自然震动、光照变化或者摄像头本身等原因而产生噪声。对噪声进行平滑是为了避免在运动和跟踪时将其检测出来。
         gray_lwpCV = cv2.cvtColor(frame_lwpCV, cv2.COLOR_BGR2GRAY)
         gray_lwpCV = cv2.GaussianBlur(gray_lwpCV, (21, 21), 0)
-        gray_lwpCV = cv2.add(gray_lwpCV,addimg,mask=mask)
 
         # 将第一帧设置为整个输入的背景
         if background is None:
@@ -60,11 +54,11 @@ def object_detection(e,d):
 
         # 显示矩形框
         # 该函数计算一幅图像中目标的轮廓
-        image, contours, hierarchy = cv2.findContours(diff.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE) 
+        _, contours, _ = cv2.findContours(diff.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE) 
         for c in contours:
 
-            # 对于矩形区域，只显示大于给定阈值的轮廓，所以一些微小的变化不会显示。对于光照不变和噪声低的摄像头可不设定轮廓最小尺寸的阈值
-            if cv2.contourArea(c) < 1500: 
+            # 对于矩形区域，只显示给定阈值的轮廓，所以一些微小的变化和背景变化不会显示。对于光照不变和噪声低的摄像头可不设定轮廓最小尺寸的阈值
+            if cv2.contourArea(c) < 1500 and cv2.contourArea(c) > 600000: 
                 continue
 
             # 该函数计算矩形的边界框
@@ -81,32 +75,31 @@ def object_detection(e,d):
             background = gray_lwpCV
         
         # 显示结果图像
-        #cv2.imshow('contours', frame_lwpCV)
-        #cv2.imshow('gary', gray_lwpCV)
-        #cv2.imshow('dis', diff)
+        cv2.imshow('contours', frame_lwpCV)
+        cv2.imshow('gary', gray_lwpCV)
+        cv2.imshow('dis', diff)
 
-        # 按'esc'健退出循环
-        # key = cv2.waitKey(1) & 0xFF
-        # if key == ord('q'):
-        #     d['status'] = False
-        #     break
+        #按'q'健退出循环
+        key = cv2.waitKey(1) & 0xFF
+        if key == ord('q'):
+            d['status'] = False
+            break
 
-    # When everything done, release the capture
+    #处理完毕，释放所有资源
     output.release()
-    #cv2.destroyAllWindows()
+    cv2.destroyAllWindows()
+    print('完成录像，释放资源')
 
 
-def get_rtps(e,d):
+def get_video(e,d):
     # 获取网络摄像头
-    camera = cv2.VideoCapture('rtsp://admin:PSAXMJ@192.168.2.129:554/h264/ch1/main/av_stream')
-    print('成功连接摄像头')
+    camera = cv2.VideoCapture('tcp://192.168.2.127:8090')
     # 测试用,查看视频size,fps
     size = (int(camera.get(cv2.CAP_PROP_FRAME_WIDTH)),
             int(camera.get(cv2.CAP_PROP_FRAME_HEIGHT)))
     fps = int(camera.get(cv2.CAP_PROP_FPS))
 
-    if fps > 30:
-        fps = 14
+    print('成功连接摄像头 size:{},fps:{}'.format(str(size),str(fps)))
 
     d['size'] = size
     d['fps'] = fps
@@ -136,7 +129,7 @@ if __name__ == '__main__':
         d = manager.dict()
         d['status'] = True
 
-        get_image = mp.Process(target = get_rtps,args = (e,d))
+        get_image = mp.Process(target = get_video,args = (e,d))
         get_image.start()
         print('视频捕获进程启动')
 
@@ -144,4 +137,3 @@ if __name__ == '__main__':
 
         get_image.join()
         print('程序结束')
-        
---
+++ 图像测试.py
@@ -0,0 +1,28 @@
+import cv2
+
+
+if __name__ == '__main__':
+
+    cv2.namedWindow('contours',0)
+
+    camera = cv2.VideoCapture('tcp://192.168.2.127:8090')
+    # 测试用,查看视频size,fps
+    size = (int(camera.get(cv2.CAP_PROP_FRAME_WIDTH)),
+            int(camera.get(cv2.CAP_PROP_FRAME_HEIGHT)))
+    fps = int(camera.get(cv2.CAP_PROP_FPS))
+
+    print('成功连接摄像头 size:{},fps:{}'.format(str(size),str(fps)))
+
+    while camera.isOpened():
+        # 读取视频流
+        ret, frame_lwpCV = camera.read()
+        if ret == True:
+            video = frame_lwpCV
+
+        key = cv2.waitKey(1) & 0xFF
+        if key == ord('q'):
+            break
+
+        cv2.imshow('contours', video)
+
+    cv2.destroyAllWindows()
